f08a8eeb31c9f5fe56cb2648fbddaa1825b9083e
TarasKarabas <taraskyparenko@gmail.com>
2023-04-07 18:35:13 +1030

    Initial Commit

---
+++ Rock_Scissors_Paper.GAME/Rock_Scissors_Paper_GAMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Scissors_Paper_GAMEApp.swift
+//  Rock_Scissors_Paper.GAME
+//
+//  Created by Taras Kyparenko on 7/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Scissors_Paper_GAMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
